60e68dbdecce5fd15a2a0b77d6f54df855fe5e7d
Wilhelman <guillermogarsub@gmail.com>
2018-09-18 18:57:06 +0200

    Testing ImGui

--- Tabula Rasa Engine/3D_Engine/trMainScene.cpp
+++ Tabula Rasa Engine/3D_Engine/trMainScene.cpp
@@ -3,6 +3,8 @@
 #include "trApp.h"
 #include "trMainScene.h"
 
+#include "imgui.h"
+
 #define VSYNC true
 
 trMainScene::trMainScene() : trModule()
@@ -25,9 +27,37 @@ bool trMainScene::Awake(pugi::xml_node& config)
 
 // Called before the first frame
 bool trMainScene::Start()
-{
+{/*
 	LOG("Main Scene start");
 
+	ImGui::Begin("My First Tool", true, ImGuiWindowFlags_MenuBar);
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Open..", "Ctrl+O")) {  }
+			if (ImGui::MenuItem("Save", "Ctrl+S")) { }
+			if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenuBar();
+	}
+
+	// Edit a color (stored as ~4 floats)
+	ImGui::ColorEdit4("Color", my_color);
+
+	// Plot some values
+	const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
+	ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
+
+	// Display contents in a scrolling region
+	ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+	ImGui::BeginChild("Scrolling");
+	for (int n = 0; n < 50; n++)
+		ImGui::Text("%04d: Some text", n);
+	ImGui::EndChild();
+	ImGui::End();
+	*/
 	return true;
 }
 
