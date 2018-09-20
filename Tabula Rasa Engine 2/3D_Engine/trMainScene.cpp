2c1181e3febd28fb9d0f716a96d3496194b38390
Wilhelman <guillermogarsub@gmail.com>
2018-09-20 17:02:07 +0200

    ImGui integrated andworking with proj2

--- Tabula Rasa Engine 2/3D_Engine/trMainScene.cpp
+++ Tabula Rasa Engine 2/3D_Engine/trMainScene.cpp
@@ -3,10 +3,10 @@
 #include "trApp.h"
 #include "trMainScene.h"
 #include "trCamera3D.h"
-#include "imgui.h"
-
-#include "Primitive.h"
+#include "trWindow.h"
+#include "trRenderer3D.h"
 #include "PhysBody3D.h"
+#include "trInput.h"
 
 #define VSYNC true
 
@@ -34,27 +34,72 @@ bool trMainScene::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
+	// Setup Dear ImGui binding
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	io = ImGui::GetIO();
+	(void)io;
+
+	ImGui_ImplSDL2_InitForOpenGL(App->win->window, App->render->context);
+	ImGui_ImplOpenGL2_Init();
+
+	// Setup style
+	ImGui::StyleColorsDark();
+
 	return true;
 }
 
 // Called each loop iteration
 bool trMainScene::PreUpdate(float dt)
 {
+
+	if (quit)
+		return false;
+
 	return true;
 }
 
 bool trMainScene::Update(float dt)
 {
 
-	Plane p(0, 1, 0, 0);
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->win->window);
+	ImGui::NewFrame();
+
+
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+	//just testing
+	ShowTheFirstTabulaRasaWindow();
+
+
 	p.axis = true;
-	p.Render();
+	
 
 	return true;
 }
 
 bool trMainScene::PostUpdate(float dt)
 {
+	// Rendering
+
+	
+
+	ImGui::Render();
+	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+	//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+	glClear(GL_COLOR_BUFFER_BIT);
+	//glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+
+	//rendering the plane
+	p.Render();
+	
+	
+
 	return true;
 }
 
@@ -62,6 +107,14 @@ bool trMainScene::PostUpdate(float dt)
 bool trMainScene::CleanUp()
 {
 	LOG("Cleaning up trMainScene");
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_GL_DeleteContext(App->render->context);
+	SDL_DestroyWindow(App->win->window);
+	SDL_Quit();
+
 	return true;
 }
 
@@ -76,4 +129,36 @@ bool trMainScene::Save(pugi::xml_node& data) const
 {
 
 	return true;
+}
+
+void trMainScene::ShowTheFirstTabulaRasaWindow()
+{
+	static float f = 0.0f;
+	static int counter = 0;
+
+	//
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ShowExampleMenuFile();
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Window"))
+		{
+			ImGui::Checkbox("Demo Window (Ctrl + W)", &show_demo_window);
+			ImGui::EndMenu();
+		}
+		else if (App->input->GetKey(SDL_SCANCODE_LCTRL) == KEY_REPEAT && App->input->GetKey(SDL_SCANCODE_W) == KEY_DOWN)
+			show_demo_window = !show_demo_window;
+
+		ImGui::EndMainMenuBar();
+	}
+}
+
+void trMainScene::ShowExampleMenuFile()
+{
+	if (ImGui::MenuItem("Exit", "Alt+F4"))
+		quit = true;
+
 }
--- Tabula Rasa Engine 2/3D_Engine/trMainScene.h
+++ Tabula Rasa Engine 2/3D_Engine/trMainScene.h
@@ -4,6 +4,14 @@
 #include "SDL/include/SDL.h"
 #include "trPoint.h"
 #include "trModule.h"
+#include "Primitive.h"
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl2.h"
+#include <stdio.h>
+#include "SDL\include\SDL.h"
+#include "SDL\include\SDL_opengl.h"
 
 struct PhysBody3D;
 struct PhysMotor3D;
@@ -35,9 +43,21 @@ public:
 	bool Load(pugi::xml_node&);
 	bool Save(pugi::xml_node&) const;
 
+private:
+	void ShowTheFirstTabulaRasaWindow();
+	void ShowExampleMenuFile();
+
+private:
+	Plane p = Plane(0, 1, 0, 0);
+
 public:
 	PhysBody3D* sensor;
 
+	bool show_demo_window = false;
+	bool quit = false;
+
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	ImGuiIO io;
 };
 
 #endif // __trRENDER_H__
