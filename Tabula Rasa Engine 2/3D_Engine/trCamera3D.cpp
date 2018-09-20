386c4ac8529941c8928f39d7dffb39daba86c290
Wilhelman <guillermogarsub@gmail.com>
2018-09-20 16:47:18 +0200

    Camera working in 3D

--- Tabula Rasa Engine 2/3D_Engine/trCamera3D.cpp
+++ Tabula Rasa Engine 2/3D_Engine/trCamera3D.cpp
@@ -41,7 +41,7 @@ bool trCamera3D::Update(float dt)
 {
 	// Implement a debug camera with keys and mouse
 	// Now we can make this movememnt frame rate independant!
-	/*
+	
 	vec3 newPos(0, 0, 0);
 	float speed = 3.0f * dt;
 	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
@@ -96,7 +96,7 @@ bool trCamera3D::Update(float dt)
 
 		Position = Reference + Z * length(Position);
 	}
-	*/
+	
 	// Recalculate matrix -------------
 	CalculateViewMatrix();
 
