0c7a32beb82bcc87722e870b602a8ff92ad179c5
nintervik <vmasogarcia@gmail.com>
2018-09-25 17:44:20 +0200

    more info to hardware

--- Tabula Rasa Engine/3D_Engine/trHardware.cpp
+++ Tabula Rasa Engine/3D_Engine/trHardware.cpp
@@ -6,14 +6,26 @@ trHardware::trHardware()
 {
 	SDL_version sdl_version;
 	SDL_GetVersion(&sdl_version);
+	hw_info.sdl_version_num[0] = sdl_version.major;
+	hw_info.sdl_version_num[1] = sdl_version.minor;
+	hw_info.sdl_version_num[2] = sdl_version.patch;
 
-	version_major = sdl_version.major;
-	version_minor = sdl_version.minor;
-	version_patch = sdl_version.patch;
-
-
-
+	hw_info.cpu_count = SDL_GetCPUCount();
+	hw_info.cache_line_size = SDL_GetCPUCacheLineSize();
 	
+	hw_info.system_ram = SDL_GetSystemRAM();
+	hw_info.has_3d_now = SDL_Has3DNow();
+	hw_info.has_avx = SDL_HasAVX();
+	hw_info.has_avx2 = SDL_HasAVX2();
+	hw_info.has_alti_vec = SDL_HasAltiVec();
+	hw_info.has_mmx = SDL_HasMMX();
+	hw_info.has_rtdcs =	SDL_HasRDTSC();
+	hw_info.has_sse = SDL_HasSSE();
+	hw_info.has_sse2 = SDL_HasSSE2();
+	hw_info.has_sse3 = SDL_HasSSE3();
+	hw_info.has_sse41 = SDL_HasSSE41();
+	hw_info.has_sse42 = SDL_HasSSE42();
+
 }
 
 trHardware::~trHardware()
--- Tabula Rasa Engine/3D_Engine/trHardware.h
+++ Tabula Rasa Engine/3D_Engine/trHardware.h
@@ -15,8 +15,24 @@ public:
 
 private:
 
-	int version_major, version_minor, version_patch;
-
+	struct HWInfo
+	{
+		int sdl_version_num[3];
+		unsigned int cpu_count = 0u;
+		unsigned int cache_line_size = 0u;
+		int system_ram = 0;
+		bool has_3d_now = false;
+		bool has_avx = false;
+		bool has_avx2 = false;
+		bool has_alti_vec = false;
+		bool has_mmx = false;
+		bool has_rtdcs = false;
+		bool has_sse = false;
+		bool has_sse2 = false;
+		bool has_sse3 = false;
+		bool has_sse41 = false;
+		bool has_sse42 = false;
+	} hw_info;
 };
 
 #endif // __trHARDWARE_H__
