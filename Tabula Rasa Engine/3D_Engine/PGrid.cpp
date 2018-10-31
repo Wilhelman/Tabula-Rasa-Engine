673f345a956039c703943a13e3e4360711f1769d
nintervik <vmasogarcia@gmail.com>
2018-10-31 18:19:13 +0100

    Fixed grid drawing

--- Tabula Rasa Engine/3D_Engine/PGrid.cpp
+++ Tabula Rasa Engine/3D_Engine/PGrid.cpp
@@ -45,76 +45,41 @@ PGrid::PGrid() : trPrimitive()
 		offset++;
 	}
 
-
-	//todo do it nicer
-	float vertices_arr[72] = { //TODO : DO IT WITH GLFLOATS!
-		//X
-		-3.f, 0.f, -3.f,
-		3.f, 0.f, -3.f,
-		-3.f, 0.f, -2.f,
-		3.f, 0.f, -2.f,
-		-3.f, 0.f, -1.f,
-		3.f, 0.f, -1.f,
-		-3.f, 0.f, 0.f,
-		3.f, 0.f, 0.f,
-		-3.f, 0.f, 1.f,
-		3.f, 0.f, 1.f,
-		-3.f, 0.f, 2.f,
-		3.f, 0.f, 2.f,
-		-3.f, 0.f, 3.f,
-		3.f, 0.f, 3.f,
-		//Z
-		-2.f, 0.f, -3.f,
-		-2.f, 0.f, 3.f,
-		-1.f, 0.f, -3.f,
-		-1.f, 0.f, 3.f,
-		0.f, 0.f, -3.f,
-		0.f, 0.f, 3.f,
-		1.f, 0.f, -3.f,
-		1.f, 0.f, 3.f,
-		2.f, 0.f, -3.f,
-		2.f, 0.f, 3.f
-	};
-
 	vertices_index = 0;
 	glGenBuffers(1, (GLuint*) &(vertices_index));
 	glBindBuffer(GL_ARRAY_BUFFER, vertices_index);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 96, vertex_array, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	uint indices_array[32];
-	uint helper = 0;
+	uint count_index = 0;
+	uint helper_count = 0;
 
-	for (uint i = 0; i < 32; i++)
+	while (count_index < 36)
 	{
-		indices_array[i] = i + helper;
-		helper = 0;
+		indices_array[count_index] = helper_count;
 
-		if (i == 18)
-			indices_array[i] = indices_array[0];
-		else if (i == 19)
-			indices_array[i] = indices_array[16];
-		else if (i == 30)
-			indices_array[i] = indices_array[1];
-		else if (i == 31)
-			indices_array[i] = indices_array[17];
-
-		if (i > 18)
-			helper++;
-	}
+		if (count_index == 18)
+			indices_array[count_index] = indices_array[0];
+		else if (count_index == 19)
+		{
+			indices_array[count_index] = indices_array[16];
+			helper_count = 17;
+		}
+		else if (count_index == 34)
+			indices_array[count_index] = indices_array[1];
+		else if (count_index == 35)
+			indices_array[count_index] = indices_array[17];
 		
+		helper_count++;
+		count_index++;
+	}
 
-	uint indices_arr[28] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0,
-		12, 1, 13 , 14, 15, 16, 17 , 18, 19, 20 , 21, 22, 23
-	};
-
-	num_index = 32;
+	num_index = 36;
 
 	indices_index = 0;
 	glGenBuffers(1, (GLuint*) &(indices_index));
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_index);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 32, indices_array, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 36, indices_array, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
