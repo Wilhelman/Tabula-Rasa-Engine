0a8529cb200ebec19f1f8639d46bcf0dbd9ccf62
nintervik <vmasogarcia@gmail.com>
2018-11-05 21:03:03 +0100

    Credited PhysFS author + initialized PhysFS

--- Tabula Rasa Engine/3D_Engine/trFileSystem.cpp
+++ Tabula Rasa Engine/3D_Engine/trFileSystem.cpp
@@ -1,10 +1,25 @@
+#include "trApp.h"
 #include "trFileSystem.h"
-#include "PhysFS\include\physfs.h"
+#include "trLog.h"
+#include "PhysFS/include/physfs.h"
 
 #pragma comment( lib, "PhysFS/libx86/physfs.lib" )
 
 trFileSystem::trFileSystem()
 {
+	this->name = "FileSystem";
+
+	// Initializing PhysFS library
+	TR_LOG("trFileSystem: initializing PhysFS library...");
+	char* base_path = SDL_GetBasePath();
+	PHYSFS_init(base_path);
+	SDL_free(base_path);
+
+	// Setting game directory to write in it
+	TR_LOG("trFileSystem: Setting game directory to write in it...");
+	PHYSFS_setWriteDir(".");
+
+	// TODO: here we should create all the needed directories here
 }
 
 trFileSystem::~trFileSystem()
