08c1bfcc7c0ceb0be79a5814ff5d2fe129106e59
Wilhelman <guillermogarsub@gmail.com>
2018-11-07 17:46:20 +0100

    Importe now only generate .tr files

--- Tabula Rasa Engine/3D_Engine/MeshImporter.cpp
+++ Tabula Rasa Engine/3D_Engine/MeshImporter.cpp
@@ -61,7 +61,7 @@ bool MeshImporter::Import(const char * path, std::string & output_file)
 		scene_vertices.clear();
 		cursor_data = nullptr;
 		material_data = nullptr;
-		ImportNodesRecursively(scene->mRootNode, scene, App->main_scene->GetRoot(), (char*)path);
+		ImportNodesRecursively(scene->mRootNode, scene, /*App->main_scene->GetRoot(),*/ (char*)path);
 
 		// Camera AABB stuff
 		if (scene->mRootNode->mNumChildren == 1) { // if only one mesh, get the bounding_box of the last mesh
@@ -108,14 +108,10 @@ bool MeshImporter::Import(const void * buffer, uint size, std::string & output_f
 
 
 
-void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * scene, GameObject * parent_go, char* file_path)
+void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * scene, char* file_path)
 {
-
-	GameObject* new_go = nullptr;
-
 	if (node->mNumMeshes > 0) //if this node have a mesh
 	{
-		new_go = App->main_scene->CreateGameObject(node->mName.C_Str(), parent_go);
 		std::string tmp = "";
 		if (file_path != nullptr) {
 			tmp = file_path;
@@ -127,8 +123,6 @@ void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * s
 			if (std::string::npos != extension)
 				tmp.erase(extension);
 			file_name = tmp;
-			//new_go->SetName(tmp.c_str());
-			model_root = new_go;
 			file_path = nullptr;
 		}
 
@@ -140,20 +134,17 @@ void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * s
 		Quat rot(rotation.x, rotation.y, rotation.z, rotation.w);
 		float3 quat_to_euler = rot.ToEulerXYZ(); // transforming it to euler to show it in inspector
 
-		ComponentTransform* transform_comp = (ComponentTransform*)new_go->CreateComponent(Component::component_type::COMPONENT_TRANSFORM);
-		transform_comp->Setup(float3(translation.x, translation.y, translation.z), float3(scaling.x, scaling.y, scaling.z), rot);
-
 		mesh_data = new Mesh(); // our mesh
 
 		aiMesh* new_mesh = scene->mMeshes[node->mMeshes[0]];
 
 		// Getting texture material if needed	
-		if (scene->mMaterials[new_mesh->mMaterialIndex] != nullptr) {
+		/*if (scene->mMaterials[new_mesh->mMaterialIndex] != nullptr) {
 			if (material_data == nullptr)
 				material_data = LoadTexture(scene->mMaterials[new_mesh->mMaterialIndex], new_go);
 			else
 				material_data = (ComponentMaterial*)new_go->CreateComponent(Component::component_type::COMPONENT_MATERIAL, material_data);
-		}
+		}*/
 
 		// Vertex copy
 		mesh_data->vertex_size = new_mesh->mNumVertices;
@@ -196,24 +187,128 @@ void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * s
 			}
 		}
 
-		// Generating bounding box
-		new_go->bounding_box = AABB(float3(0.f, 0.f, 0.f), float3(0.f, 0.f, 0.f));
-		new_go->bounding_box.Enclose((float3*)mesh_data->vertices, mesh_data->vertex_size);
+		std::string output_file;
 
-		ComponentMesh* mesh_comp = (ComponentMesh*)new_go->CreateComponent(Component::component_type::COMPONENT_MESH);
-		mesh_comp->SetMesh(mesh_data);
+		SaveMeshFile(node->mName.C_Str(), mesh_data, output_file);
 
-		//testing zone
-		// check if there is already a file
-		// todo finish this
-		SaveMeshFile(new_go->GetName(), mesh_data);
+		LoadMeshFile(output_file.c_str());
 
+		RELEASE(mesh_data);
 	}
 	for (uint i = 0; i < node->mNumChildren; i++)
-		ImportNodesRecursively(node->mChildren[i], scene, new_go, file_path);
+		ImportNodesRecursively(node->mChildren[i], scene, file_path);
+}
+
+/* SAVE ZONE
+
+void MeshImporter::ImportNodesRecursively(const aiNode * node, const aiScene * scene, GameObject * parent_go, char* file_path)
+{
+
+GameObject* new_go = nullptr;
+
+if (node->mNumMeshes > 0) //if this node have a mesh
+{
+new_go = App->main_scene->CreateGameObject(node->mName.C_Str(), parent_go);
+std::string tmp = "";
+if (file_path != nullptr) {
+tmp = file_path;
+// Let's get the file name to print it in inspector:
+const size_t last_slash = tmp.find_last_of("\\/");
+if (std::string::npos != last_slash)
+tmp.erase(0, last_slash + 1);
+const size_t extension = tmp.rfind('.');
+if (std::string::npos != extension)
+tmp.erase(extension);
+file_name = tmp;
+//new_go->SetName(tmp.c_str());
+model_root = new_go;
+file_path = nullptr;
+}
+
+// Calculate the position, scale and rotation
+aiVector3D translation;
+aiVector3D scaling;
+aiQuaternion rotation;
+node->mTransformation.Decompose(scaling, rotation, translation);
+Quat rot(rotation.x, rotation.y, rotation.z, rotation.w);
+float3 quat_to_euler = rot.ToEulerXYZ(); // transforming it to euler to show it in inspector
+
+ComponentTransform* transform_comp = (ComponentTransform*)new_go->CreateComponent(Component::component_type::COMPONENT_TRANSFORM);
+transform_comp->Setup(float3(translation.x, translation.y, translation.z), float3(scaling.x, scaling.y, scaling.z), rot);
+
+mesh_data = new Mesh(); // our mesh
+
+aiMesh* new_mesh = scene->mMeshes[node->mMeshes[0]];
+
+// Getting texture material if needed
+if (scene->mMaterials[new_mesh->mMaterialIndex] != nullptr) {
+if (material_data == nullptr)
+material_data = LoadTexture(scene->mMaterials[new_mesh->mMaterialIndex], new_go);
+else
+material_data = (ComponentMaterial*)new_go->CreateComponent(Component::component_type::COMPONENT_MATERIAL, material_data);
+}
+
+// Vertex copy
+mesh_data->vertex_size = new_mesh->mNumVertices;
+mesh_data->vertices = new float[mesh_data->vertex_size * 3];
+memcpy(mesh_data->vertices, new_mesh->mVertices, sizeof(float) * mesh_data->vertex_size * 3);
+
+// Data for the bounding box of all the meshes
+for (uint i = 0; i < mesh_data->vertex_size; i++) {
+scene_vertices.push_back(float3(mesh_data->vertices[i], mesh_data->vertices[i + 1], mesh_data->vertices[i + 2]));
+}
+scene_num_vertex += mesh_data->vertex_size;
+
+// UVs copy
+if (new_mesh->HasTextureCoords(0)) {//i?
+mesh_data->size_uv = new_mesh->mNumVertices;
+mesh_data->uvs = new float[mesh_data->size_uv * 2];
+for (int i = 0; i < mesh_data->size_uv; i++) {
+mesh_data->uvs[i * 2] = new_mesh->mTextureCoords[0][i].x;
+mesh_data->uvs[i * 2 + 1] = new_mesh->mTextureCoords[0][i].y;
+}
+}
+else {
+mesh_data->size_uv = 0;
+mesh_data->uvs = nullptr;
+}
+
+// Index copy
+if (new_mesh->HasFaces())
+{
+mesh_data->face_size = new_mesh->mNumFaces;
+mesh_data->index_size = new_mesh->mNumFaces * 3;
+mesh_data->indices = new uint[mesh_data->index_size]; // assume each face is a triangle
+for (uint i = 0; i < new_mesh->mNumFaces; ++i)
+{
+if (new_mesh->mFaces[i].mNumIndices != 3) {
+TR_LOG("WARNING, geometry face with != 3 indices!");
+}
+else
+memcpy(&mesh_data->indices[i * 3], new_mesh->mFaces[i].mIndices, 3 * sizeof(uint));
+}
+}
+
+// Generating bounding box
+new_go->bounding_box = AABB(float3(0.f, 0.f, 0.f), float3(0.f, 0.f, 0.f));
+new_go->bounding_box.Enclose((float3*)mesh_data->vertices, mesh_data->vertex_size);
+
+ComponentMesh* mesh_comp = (ComponentMesh*)new_go->CreateComponent(Component::component_type::COMPONENT_MESH);
+mesh_comp->SetMesh(mesh_data);
+
+//testing zone
+// check if there is already a file
+// todo finish this
+SaveMeshFile(new_go->GetName(), mesh_data);
+
+}
+for (uint i = 0; i < node->mNumChildren; i++)
+ImportNodesRecursively(node->mChildren[i], scene, new_go, file_path);
 
 
 }
+
+*/
 
 
 ComponentMaterial * MeshImporter::LoadTexture(aiMaterial* material, GameObject* go)
@@ -252,7 +347,7 @@ ComponentMaterial * MeshImporter::LoadTexture(aiMaterial* material, GameObject* 
 }
 
 
-bool MeshImporter::SaveMeshFile(const char* file_name, Mesh* mesh_data)
+bool MeshImporter::SaveMeshFile(const char* file_name, Mesh* mesh_data, std::string& output_file)
 {
 	uint size_indices = sizeof(uint) * mesh_data->index_size;
 	uint size_vertices = sizeof(float) * (mesh_data->vertex_size * 3);
@@ -288,9 +383,15 @@ bool MeshImporter::SaveMeshFile(const char* file_name, Mesh* mesh_data)
 	tmp_str.append(".tr"); // adding our own format extension
 
 	App->file_system->WriteInFile(tmp_str.c_str(), cursor, size);
+	output_file = tmp_str;
 
 	// deleting useless data
 	RELEASE_ARRAY(data);
 
 	return true;
+}
+
+bool MeshImporter::LoadMeshFile(const char * file_path)
+{
+	return false;
 }
