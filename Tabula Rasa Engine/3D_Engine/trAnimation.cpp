1f80eba9c27e706835138769701eb626e1849e68
nintervik <vmasogarcia@gmail.com>
2018-12-19 11:45:50 +0100

    Solved animation movement bug with frame count

--- Tabula Rasa Engine/3D_Engine/trAnimation.cpp
+++ Tabula Rasa Engine/3D_Engine/trAnimation.cpp
@@ -124,11 +124,9 @@ void trAnimation::MoveAnimationForward(float t)
 	{
 		ResourceAnimation::BoneTransformation* transform = animable_data_map.find(animable_gos[i])->second;
 
-		// Finding the previous and next frame in relation with the given time t
 		if (transform)
 		{
-			float3 pos;
-			float3 scale;
+			float3 pos, scale;
 			Quat rot;
 
 			animable_gos[i]->GetTransform()->GetLocalPosition(&pos, &scale, &rot);
@@ -137,23 +135,28 @@ void trAnimation::MoveAnimationForward(float t)
 			float* next_pos = nullptr;
 			float time_pos_percentatge = 0.0f;
 
+			float* prev_scale = nullptr;
+			float* next_scale = nullptr;
+			float time_scale_percentatge = 0.0f;
+
 			float* prev_rot = nullptr;
 			float* next_rot = nullptr;
 			float time_rot_percentatge = 0.0f;
 
-			if (transform->positions.count > i)
-			{
-				// Positions
-				float max_pos_time = 0.0f;
-				float min_pos_time = 0.0f;
+			float max_time = 0.0f;
+			float min_time = 0.0f;
 
-				// Finding next and prev pos
+			// -------- FINDING NEXT AND PREVIOUS TRANSFORMATION IN RELATIONS WITH THE GIVEN TIME (t) --------
+
+			// Finding next and previous positions	
+			if (transform->positions.count > i)
+			{				
 				for (uint j = 0; j < transform->positions.count; j += 3)
 				{
 					if (prev_pos != nullptr && next_pos != nullptr) // if prev and next frames have been found we stop
 					{
-						float time_interval = max_pos_time - min_pos_time;
-						time_pos_percentatge = (t - min_pos_time) / time_interval;
+						float time_interval = max_time - min_time;
+						time_pos_percentatge = (t - min_time) / time_interval;
 						break;
 					}
 
@@ -164,33 +167,62 @@ void trAnimation::MoveAnimationForward(float t)
 						break;
 					}
 
-					/*if (transform->positions.time[j] < t) // previous frame has been found
-					{
-						min_pos_time = transform->positions.time[j];
-						prev_pos = &transform->positions.value[j];
-					}*/
-					
 					if (transform->positions.time[j] > t) // next frame has been found
 					{
-						max_pos_time = transform->positions.time[j];
+						max_time = transform->positions.time[j];
 						next_pos = &transform->positions.value[j];
 
 						prev_pos = &transform->positions.value[j - 3];
-						min_pos_time = transform->positions.time[j - 3];
+						min_time = transform->positions.time[j - 3];
 					}
 				}
+			}
 
-				// Rotations
-				float max_rot_time = 0.0f;
-				float min_rot_time = 0.0f;
+			// Finding next and previous scalings
+			if (transform->scalings.count > i)
+			{
+				max_time = 0.0f;
+				min_time = 0.0f;
 
-				// Finding next and prev pos
+				for (uint j = 0; j < transform->scalings.count; j += 3)
+				{
+					if (prev_scale != nullptr && next_scale != nullptr) // if prev and next frames have been found we stop
+					{
+						float time_interval = max_time - min_time;
+						time_scale_percentatge = (t - min_time) / time_interval;
+						break;
+					}
+
+					if (t == transform->scalings.time[j]) // in this case interpolation won't be done
+					{
+						prev_scale = &transform->scalings.value[j];
+						next_scale = prev_scale;
+						break;
+					}
+
+					if (transform->scalings.time[j] > t) // next frame has been found
+					{
+						max_time = transform->scalings.time[j];
+						next_scale = &transform->scalings.value[j];
+
+						prev_scale = &transform->scalings.value[j - 3];
+						min_time = transform->scalings.time[j - 3];
+					}
+				}
+			}
+				
+			// Finding next and previous rotations
+			if (transform->rotations.count > i)
+			{
+				max_time = 0.0f;
+				min_time = 0.0f;
+
 				for (uint j = 0; j < transform->rotations.count; j += 4)
 				{
 					if (prev_rot != nullptr && next_rot != nullptr) // if prev and next frames have been found we stop
 					{
-						float time_interval = max_rot_time - min_rot_time;
-						time_rot_percentatge = (t - min_rot_time) / time_interval;
+						float time_interval = max_time - min_time;
+						time_rot_percentatge = (t - min_time) / time_interval;
 						break;
 					}
 
@@ -203,56 +235,55 @@ void trAnimation::MoveAnimationForward(float t)
 
 					if (transform->rotations.time[j] > t) // next frame has been found
 					{
-						max_rot_time = transform->rotations.time[j];
+						max_time = transform->rotations.time[j];
 						next_rot = &transform->rotations.value[j];
 
 						prev_rot = &transform->rotations.value[j - 4];
-						min_rot_time = transform->rotations.time[j - 4];
+						min_time = transform->rotations.time[j - 4];
 					}
 				}
+			}
 
-				// -------- INTERPOLATIONS CALCULATIONS --------
+			// -------- INTERPOLATIONS CALCULATIONS --------
 
-				if (prev_pos != nullptr && next_pos != nullptr)
+			// Interpolating positions
+			if (prev_pos != nullptr && next_pos != nullptr)
 				{
 					float3 prev_pos_lerp(prev_pos[0], prev_pos[1], prev_pos[2]);
 
 					if (prev_pos != next_pos)
 					{
-						// Positions
 						float3 next_pos_lerp(next_pos[0], next_pos[1], next_pos[2]);
-
 						pos = float3::Lerp(prev_pos_lerp, next_pos_lerp, time_pos_percentatge);
 					}
-					else // if prev and next pos are equal, we don't need to interpolate
-						pos = prev_pos_lerp;
 				}
 
-				if (prev_rot != nullptr && next_rot != nullptr)
+			// Interpolating scalings
+			if (prev_scale != nullptr && next_scale != nullptr)
+			{
+				float3 prev_scale_lerp(prev_scale[0], prev_scale[1], prev_scale[2]);
+
+				if (prev_scale != next_scale)
+				{
+					float3 next_scale_lerp(next_scale[0], next_scale[1], next_scale[2]);
+					scale = float3::Lerp(prev_scale_lerp, next_scale_lerp, time_scale_percentatge);
+				}
+			}
+
+			// Interpolating rotations
+			if (prev_rot != nullptr && next_rot != nullptr)
 				{
 					Quat prev_rot_lerp(prev_rot[0], prev_rot[1], prev_rot[2], prev_rot[3]);
 
 					if (prev_rot != next_rot)
 					{
-						// Rotations
 						Quat next_rot_lerp(next_rot[0], next_rot[1], next_rot[2], next_rot[3]);
-
 						rot = Quat::Slerp(prev_rot_lerp, next_rot_lerp, time_rot_percentatge);
 					}
-					else // if prev and next rot are equal, we don't need to interpolate
-						rot = prev_rot_lerp;
-
-					animable_gos[i]->GetTransform()->Setup(pos, scale, rot);
 				}
 
-				prev_pos = nullptr;
-				next_pos = nullptr;
-				time_pos_percentatge = 0.0f;
-
-				prev_rot = nullptr;
-				next_rot = nullptr;
-				time_rot_percentatge = 0.0f;
-			}
+			// Setting up final interpolated transform
+			animable_gos[i]->GetTransform()->Setup(pos, scale, rot);	
 		}
 	}
 }
